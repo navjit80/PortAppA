0aed91cf5104d7e450fa309d76795316a69351bb
James Richardson <3richj71@solent.ac.uk>
2021-11-06 11:26:52 +0000

    Dock Service was name was wrong

--- porta-model/src/main/java/com/portapp/model/dock/dao/DockDAO.java
+++ porta-model/src/main/java/com/portapp/model/dock/dao/DockDAO.java
@@ -4,10 +4,35 @@
  */
 package com.portapp.model.dock.dao;
 
+import com.portapp.model.dock.dto.Dock;
+import com.portapp.model.dock.dto.DockStatus;
+import java.util.List;
+
 /**
  *
  * @author 1985j
  */
 public interface DockDAO {
+
+    public Dock findById(Long id);
+    
+    public Dock save(Dock dock);
+    
+    public List<Dock> findAll();
+    
+    public List<Dock> findDockByPortId(Long id);
+    
+    public List<Dock> findDockByDockStatus(DockStatus status);
+    
+    public void deleteById(long id);
+
+    public void delete(Dock dock);
+
+    public void deleteAll();
+
+    public List<Dock> findByName(String dockName);
+    
+    public Dock findByUuid(String uuid);
+    
     
 }
